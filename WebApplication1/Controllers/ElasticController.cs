fd7944e5023a4e399ad8f96d49a1bf547c866ff5
Tomer <tptomerpeled@gmail.com>
2018-06-01 01:43:53 +0300

    first commit authenticating to server getting 404's

---
+++ WebApplication1/Controllers/ElasticController.cs
@@ -0,0 +1,53 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebApplication1.Controllers
+{
+    public class ElasticController : ApiController
+    {
+
+        private static ConnectionSettings connSettings = new ConnectionSettings(new Uri("http://kb-ls4qlqhio6hqu.westeurope.cloudapp.azure.com:5601/"));
+
+        private static ElasticClient client;
+
+       
+
+        public ElasticController()
+        {
+            connSettings.DisableDirectStreaming().PrettyJson()
+                .BasicAuthentication("elastic", "0508386300T");
+            client = new ElasticClient(connSettings);
+        }
+
+
+        public static ConnectionSettings ConnSettings => connSettings;
+
+        [HttpGet]
+       // [Route("Get_Index")]
+        public IHttpActionResult GET(String Index)
+        {
+            var response = client.IndexExists(Index).Exists;
+            if (response == false)
+            {
+                return NotFound();
+            }
+            return Ok(response);
+
+            //
+        }
+        //Work in Progress not working yet
+        [HttpPost]
+        public IHttpActionResult POST([FromBody]dynamic Read, String index)
+        {
+            string q = Read.q();
+            return Ok(q);
+        }
+    }
+}
---
+++ WebApplication1/Models/Reading.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication1.Controllers
+{
+    public class Reading
+    {
+        public string user_id { get; set; }
+        public string user_name { get; set; }
+        public IEnumerable<string> measurments { get; set; }
+    }
+}
